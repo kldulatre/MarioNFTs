1327ea4081c0810fb53b3832a314570ff3143344
Kenjie Lloyd Dulatre <kdulatre@up.edu.ph>
2021-12-09 14:12:36 +0800

    community mobile responsiveness

--- components/Community.vue
+++ components/Community.vue
@@ -1,17 +1,25 @@
 <template>
-	<div class="relative mt-40">
-		<div class="z-1 w-full background"></div>
-		<div class="z-10 absolute bottom-0 w-full">
-			<div class="container mx-auto m-12">
-				<div class="flex items-end">
-					<img class="w-1/2 object-contain h-96" src="images/community.png" alt="community" />
-					<div class="w-1/2 flex-1 m-8 space-y-6">
-						<Header :data="'Join Our Community'" />
-						<SubBody :data="'Be part of our data amazing community and stay tuned for the news.'" />
-						<YellowButton :data="'Join us'" />
+	<div>
+		<div class="hidden sm:block relative mt-40">
+			<div class="hidden sm:block z-1 w-full background"></div>
+			<div class="hidden sm:block z-10 absolute bottom-0 w-full">
+				<div class="container mx-auto m-12">
+					<div class="flex items-end">
+						<img class="w-1/2 object-contain h-96" src="images/community.png" alt="community" />
+						<div class="w-1/2 flex-1 m-8 space-y-6">
+							<Header :data="'Join Our Community'" />
+							<SubBody :data="'Be part of our data amazing community and stay tuned for the news.'" />
+							<YellowButton :data="'Join us'" />
+						</div>
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="sm:hidden background2 flex flex-col items-center space-y-4 py-8 mt-16">
+			<Header class="text-center" :data="'Join Our Community'" />
+			<img class="object-contain h-60" src="images/community.png" alt="community" />
+			<SubBody class="text-center" :data="'Be part of our data amazing community and stay tuned for the news.'" />
+			<YellowButton :data="'Join us'" />
 		</div>
 	</div>
 </template>
@@ -24,8 +32,11 @@ export default {};
 .container {
 	max-width: 70rem;
 }
-.background{
+.background {
 	height: 23rem;
+	background-color: #c93535;
+}
+.background2 {
 	background-color: #c93535;
 }
 </style>
--- components/Enemies.vue
+++ components/Enemies.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex sm:flex-row flex-col sm:border sm:border-opacity-25 sm:border-white sm:rounded-2xl sm:p-12 p-8 sm:px-16 background">
 		<div class="hidden sm:inline-block sm:w-1/2 space-y-8 flex flex-col justify-center">
-			<Header :data="'Enemies and obstacles'" class="w-96"></Header>
+			<Header :data="'Enemies and Obstacles'" class="w-96"></Header>
 			<SubBody :data="description" />
 			<YellowButton :data="'Check Enemies'" />
 		</div>
@@ -9,7 +9,7 @@
 			<img src="images/enemies.png" class="h-96" alt="enemies" />
 		</div>
 		<div class="sm:hidden flex flex-col items-center space-y-6">
-			<Header :data="'Enemies and obstacles'" class="sm:w-96 text-center"></Header>
+			<Header :data="'Enemies and Obstacles'" class="sm:w-96 text-center"></Header>
 			<img src="images/enemies.png" class="sm:h-96 h-60" alt="enemies" />
 			<SubBody class="text-center" :data="description" />
 			<YellowButton :data="'Check Enemies'" />
